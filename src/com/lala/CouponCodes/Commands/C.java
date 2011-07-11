29b09b9161f03900e426ed65d591f5eecd0d579f
Mike <dontlookbehindyou@live.com>
2011-07-10 18:29:47 -0700

    Add void add() for /c add

--- src/com/lala/CouponCodes/Commands/C.java
+++ src/com/lala/CouponCodes/Commands/C.java
@@ -18,19 +18,24 @@ public class C implements CommandExecutor {
 			sender.sendMessage("You need to be a player!");
 			return true;
 		}else{
+			Player player = (Player) sender;
 			if (args[0].equalsIgnoreCase("add") && args.length >= 3){
-				if (args[1].equalsIgnoreCase("ic")){
+				if (args[1].equalsIgnoreCase("ic") && args.length >=4){
 					String code = args[2];
-					ic((Player) sender, args, code);
-					return true;
-				}else{
-					// /c add
+					ic(player, args, code);
+					return true;				
 				}
+				String code = args[1];
+				add(player, args, code);
+				return true;
 			}
 		}
 		return false;
 	}
 	private void ic(Player player, String[] args, String code){
-		
+		//Config stuff needs to be done first
+	}
+	private void add(Player player, String[] args, String code){
+		//Config stuff needs some work first
 	}
 }
