9674f89c200bcd2eaa8b6b55b5aa89d56366c0cb
Mike <dontlookbehindyou@live.com>
2011-07-10 20:35:44 -0700

    Added getID getAmount getTimesCanBeUsed isiConomy

--- src/com/lala/CouponCodes/Configs/Coupon.java
+++ src/com/lala/CouponCodes/Configs/Coupon.java
@@ -31,4 +31,32 @@ public class Coupon extends Configuration{
 		yml.save();
 		return;
 	}
+	public static void redeem(String code, String name){
+		final Coupon yml = getYML();
+		//redeem stuff
+	}
+	public static int getId(String code){
+		final Coupon yml = getYML();
+		int i;
+		i = (Integer) yml.getProperty("config.coupons." + code + "prize.id");
+		return i;
+	}
+	public static int getAmount(String code){
+		final Coupon yml = getYML();
+		int a;
+		a = (Integer) yml.getProperty("config.coupons." + code + "prize.amount");
+		return a;
+	}
+	public static int getTimesCanBeUsed(String code){
+		final Coupon yml = getYML();
+		int t;
+		t = (Integer) yml.getProperty("config.coupons." + code + ".timescabeused");
+		return t;
+	}
+	public static boolean isiConomy(String code){
+		final Coupon yml = getYML();
+		boolean m;
+		m = (Boolean) yml.getProperty("config.coupons." + code + ".isiConomy");
+		return m;
+	}
 }
